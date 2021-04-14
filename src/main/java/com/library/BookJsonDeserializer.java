b65a4bb41766598cdee72161d6d3d568c07f38d5
Szymon <szymon.babinski@student.put.poznan.pl>
2021-04-14 14:48:53 +0200

    Added ISBN to Books JSON Deserializer

--- src/main/java/com/library/BookJsonDeserializer.java
+++ src/main/java/com/library/BookJsonDeserializer.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.library.domain.Book;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class BookJsonDeserializer extends JsonDeserializer<Book> {
 
@@ -17,48 +18,67 @@ public class BookJsonDeserializer extends JsonDeserializer<Book> {
         JsonNode node = p.getCodec().readTree(p);
         JsonNode volumeInfo = node.get("volumeInfo");
 
-        // TODO: isbn
+        // ISBN
+        HashMap<String, String> typeIdMap = new HashMap<>();
+        if(volumeInfo.has("industryIdentifiers")){
+            JsonNode industryIdentifiers = volumeInfo.get("industryIdentifiers");
+            if(industryIdentifiers.isArray()){
+                for(final JsonNode jsonNode: industryIdentifiers){
+                    String type = jsonNode.get("type").asText();
+                    String value = jsonNode.get("identifier").asText();
+                    typeIdMap.put(type,value);
+                }
+            }
+        }
 
+        if(typeIdMap.containsKey("ISBN_13")){
+            book.setIsbn(typeIdMap.get("ISBN_13"));
+        }else{
+            String id = node.get("id").asText();
+            book.setIsbn(id);
+        }
+
+        //TITLE
         if(volumeInfo.has("title")){
             String title = volumeInfo.get("title").asText();
             book.setTitle(title);
         }
-
+        //SUBTITLE
         if (volumeInfo.has("subtitle")) {
             String subtitle = volumeInfo.get("subtitle").asText();
             book.setSubtitle(subtitle);
         }
-
+        //PUBLISHER
         if(volumeInfo.has("publisher")){
             String publisher = volumeInfo.get("publisher").asText();
             book.setPublisher(publisher);
         }
-
+        //PUBLISHED DATE
         if(volumeInfo.has("publishedDate")){
             long publishedDate = volumeInfo.get("publishedDate").asLong();
             book.setPublishedDate(publishedDate);
         }
-
+        //DESCRIPTION
         if(volumeInfo.has("description")){
             String description = volumeInfo.get("description").asText();
             book.setDescription(description);
         }
-
+        //THUMBNAIL
         if(volumeInfo.get("imageLinks").has("thumbnail")){
             String thumbnailUrl = volumeInfo.get("imageLinks").get("thumbnail").asText();
             book.setThumbnailUrl(thumbnailUrl);
         }
-
+        //LANGUAGE
         if(volumeInfo.has("language")){
             String language = volumeInfo.get("language").asText();
             book.setLanguage(language);
         }
-
+        //PREVIEW LINK
         if(volumeInfo.has("previewLink")){
             String previewLink = volumeInfo.get("previewLink").asText();
             book.setPreviewLink(previewLink);
         }
-
+        //AVERAGE RATING
         if(volumeInfo.has("averageRating")){
             double averageRating = volumeInfo.get("averageRating").asDouble();
             book.setAverageRating(averageRating);
