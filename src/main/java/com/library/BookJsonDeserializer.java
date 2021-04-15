100a02f771bbdcb2ac5106d4714d0261835b696d
Szymon <szymon.babinski@student.put.poznan.pl>
2021-04-15 12:35:15 +0200

    - Fixed published date unix - Reformatted BookJsonDeserializer - Added deserializeAsSimpleField function

--- src/main/java/com/library/BookJsonDeserializer.java
+++ src/main/java/com/library/BookJsonDeserializer.java
@@ -10,95 +10,98 @@ import com.library.domain.Book;
 import com.library.domain.Category;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.function.Function;
 
 public class BookJsonDeserializer extends JsonDeserializer<Book> {
 
     @Override
-    public Book deserialize(final JsonParser p, final DeserializationContext ctxt) throws IOException, JsonProcessingException {
+    public Book deserialize(final JsonParser p, final DeserializationContext ctxt) throws IOException{
         Book book = new Book();
         JsonNode node = p.getCodec().readTree(p);
         JsonNode volumeInfo = node.get("volumeInfo");
 
         // ISBN
         HashMap<String, String> typeIdMap = new HashMap<>();
-        if(volumeInfo.has("industryIdentifiers")){
+        if (volumeInfo.has("industryIdentifiers")) {
             JsonNode industryIdentifiers = volumeInfo.get("industryIdentifiers");
-            if(industryIdentifiers.isArray()){
-                for(final JsonNode jsonNode: industryIdentifiers){
+            if (industryIdentifiers.isArray()) {
+                for (final JsonNode jsonNode : industryIdentifiers) {
                     String type = jsonNode.get("type").asText();
                     String value = jsonNode.get("identifier").asText();
-                    typeIdMap.put(type,value);
+                    typeIdMap.put(type, value);
                 }
             }
         }
 
-        if(typeIdMap.containsKey("ISBN_13")){
+        if (typeIdMap.containsKey("ISBN_13")) {
             book.setIsbn(typeIdMap.get("ISBN_13"));
-        }else{
+        } else {
             String id = node.get("id").asText();
             book.setIsbn(id);
         }
 
+
         //TITLE
-        if(volumeInfo.has("title")){
-            String title = volumeInfo.get("title").asText();
-            book.setTitle(title);
-        }
+        deserializeAsSimpleField(volumeInfo, "title", JsonNode::asText).ifPresent(book::setTitle);
+
         //SUBTITLE
-        if (volumeInfo.has("subtitle")) {
-            String subtitle = volumeInfo.get("subtitle").asText();
-            book.setSubtitle(subtitle);
-        }
+        deserializeAsSimpleField(volumeInfo, "subtitle", JsonNode::asText).ifPresent(book::setSubtitle);
+
         //PUBLISHER
-        if(volumeInfo.has("publisher")){
-            String publisher = volumeInfo.get("publisher").asText();
-            book.setPublisher(publisher);
-        }
+        deserializeAsSimpleField(volumeInfo, "publisher", JsonNode::asText).ifPresent(book::setPublisher);
+
         //PUBLISHED DATE
-        if(volumeInfo.has("publishedDate")){
-            long publishedDate = volumeInfo.get("publishedDate").asLong();
-            book.setPublishedDate(publishedDate);
-        }
-        //DESCRIPTION
-        if(volumeInfo.has("description")){
-            String description = volumeInfo.get("description").asText();
-            book.setDescription(description);
-        }
-        //PAGE COUNT
-        if(volumeInfo.has("pageCount")){
-            int pageCount = volumeInfo.get("pageCount").asInt();
-            book.setPageCount(pageCount);
-        }
-        //THUMBNAIL
-        if(volumeInfo.get("imageLinks").has("thumbnail")){
-            String thumbnailUrl = volumeInfo.get("imageLinks").get("thumbnail").asText();
-            book.setThumbnailUrl(thumbnailUrl);
-        }
-        //LANGUAGE
-        if(volumeInfo.has("language")){
-            String language = volumeInfo.get("language").asText();
-            book.setLanguage(language);
-        }
-        //PREVIEW LINK
-        if(volumeInfo.has("previewLink")){
-            String previewLink = volumeInfo.get("previewLink").asText();
-            book.setPreviewLink(previewLink);
-        }
-        //AVERAGE RATING
-        if(volumeInfo.has("averageRating")){
-            double averageRating = volumeInfo.get("averageRating").asDouble();
-            book.setAverageRating(averageRating);
+        if (volumeInfo.has("publishedDate")) {
+            String publishedDate = volumeInfo.get("publishedDate").textValue();
+            if (publishedDate.length() == 4) {
+                DateFormat dateFormat = new SimpleDateFormat("yyyy");
+                try {
+                    Date date = dateFormat.parse(publishedDate);
+                    long unixTime = date.getTime();
+                    book.setPublishedDate(unixTime);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                try {
+                    Date date = dateFormat.parse(publishedDate);
+                    long unixTime = date.getTime();
+                    book.setPublishedDate(unixTime);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
+        //DESCRIPTION
+        deserializeAsSimpleField(volumeInfo, "description", JsonNode::asText).ifPresent(book::setDescription);
+
+        //PAGE COUNT
+        deserializeAsSimpleField(volumeInfo, "pageCount", JsonNode::asInt).ifPresent(book::setPageCount);
+
+        //THUMBNAIL
+        deserializeAsSimpleField(volumeInfo.get("imageLinks"), "thumbnail", JsonNode::asText).ifPresent(book::setThumbnailUrl);
+
+        //LANGUAGE
+        deserializeAsSimpleField(volumeInfo, "language", JsonNode::asText).ifPresent(book::setLanguage);
+
+        //PREVIEW LINK
+        deserializeAsSimpleField(volumeInfo, "previewLink", JsonNode::asText).ifPresent(book::setPreviewLink);
+
+        //AVERAGE RATING
+        deserializeAsSimpleField(volumeInfo, "averageRating", JsonNode::asDouble).ifPresent(book::setAverageRating);
+
         // AUTHORS
-        if(volumeInfo.has("authors")){
+        if (volumeInfo.has("authors")) {
             JsonNode authors = volumeInfo.get("authors");
             List<Author> authorsList = new ArrayList<>();
-            if(authors.isArray()){
-                for(final JsonNode jsonNode:authors){
+            if (authors.isArray()) {
+                for (final JsonNode jsonNode : authors) {
                     authorsList.add(new Author(jsonNode.textValue()));
                 }
             }
@@ -106,12 +109,12 @@ public class BookJsonDeserializer extends JsonDeserializer<Book> {
         }
 
         //CATEGORIES
-        if(volumeInfo.has("categories")){
+        if (volumeInfo.has("categories")) {
             JsonNode categories = volumeInfo.get("categories");
             List<Category> categoryList = new ArrayList<>();
-            if(categories.isArray()){
-                for(final JsonNode jsonNode:categories){
-                    categoryList.add(new Category(jsonNode.asText()));
+            if (categories.isArray()) {
+                for (final JsonNode jsonNode : categories) {
+                    categoryList.add(new Category(jsonNode.textValue()));
                 }
             }
             book.setCategories(categoryList);
@@ -119,4 +122,14 @@ public class BookJsonDeserializer extends JsonDeserializer<Book> {
 
         return book;
     }
+
+
+    private <T> Optional<T> deserializeAsSimpleField(JsonNode node, String property, Function<JsonNode, T> mapper) {
+        if (node.has(property)) {
+            return Optional.of(mapper.apply(node.get(property)));
+        } else {
+            return Optional.empty();
+        }
+    }
+
 }
