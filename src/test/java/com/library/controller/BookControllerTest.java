c22de2aaab16a7d5a217f2e9b7b281fdbcbd3c4e
Szymon <szymon.babinski@student.put.poznan.pl>
2021-04-21 18:32:35 +0200

    - Added default server port to application.properties - Added annotations to run application while testing BookControllerTest.

--- src/test/java/com/library/controller/BookControllerTest.java
+++ src/test/java/com/library/controller/BookControllerTest.java
@@ -1,11 +1,15 @@
 package com.library.controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.*;
 
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class BookControllerTest {
 
     @Test
