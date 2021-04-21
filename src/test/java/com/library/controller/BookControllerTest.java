6b7653602b9d15c20b51bb17a1e620de066e3dd6
Szymon <szymon.babinski@student.put.poznan.pl>
2021-04-21 16:51:47 +0200

    - Added test to BookService - Added RestAssured dependency do pom.xml - Created BookController tests

---
+++ src/test/java/com/library/controller/BookControllerTest.java
@@ -0,0 +1,97 @@
+package com.library.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.*;
+
+
+class BookControllerTest {
+
+    @Test
+    public void book_details_returns_200_when_expected_isbn() {
+
+        when()
+                .get("/books/{isbn}", "9780471721260").
+                then()
+                .statusCode(200)
+                .body("isbn", equalTo("9780471721260"));
+    }
+
+    @Test
+    public void book_details_returns_404_when_wrong_isbn() {
+
+        when()
+                .get("/books/{isbn}", "wrongIsbn").
+                then()
+                .statusCode(404);
+    }
+
+    @Test
+    public void books_categories_returns_200_and_correct_json_when_expected_category() {
+
+        when()
+                .get("/books/categories/{category}", "Computers").
+                then()
+                .statusCode(200)
+                .body("", hasSize(greaterThan(0)),
+                        "categories", everyItem(hasItem("Computers")));
+    }
+
+    @Test
+    public void books_categories_returns_200_and_empty_json_when_wrong_category() {
+
+        when()
+                .get("/books/categories/{category}", "wrongCategory").
+                then()
+                .statusCode(200)
+                .body("", hasSize(0));
+    }
+
+    @Test
+    public void authors_rating_returns_200_and_correct_json() {
+        when()
+                .get("/authors/ratings").
+                then()
+                .statusCode(200)
+                .body("name", notNullValue(),
+                        "averageRating", notNullValue());
+    }
+
+    @Test
+    public void books_volume_returns_200_and_correct_pageCount() {
+        when()
+                .get("/books/volume/{pageCount}", 100).
+                then()
+                .statusCode(200)
+                .body("pageCount", greaterThan(100));
+    }
+
+    @Test
+    public void books_volume_returns_404_while_wrong_pageCount() {
+        when()
+                .get("/books/volume/{pageCount}", 999999)
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    public void best_books_returns_correct_json_while_correct_pagesPerHour_and_hoursDaily() {
+        int pagesPerMonth = 5 * 10 * 30; // 5 pages per hour * 10 hours daily * 30 days
+        when()
+                .get("/books/pages/{pagesPerHour}/hours/{hoursDaily}", 5, 10)
+                .then()
+                .statusCode(200)
+                .body("pageCount", everyItem(lessThan(pagesPerMonth)),
+                        "pageCount", everyItem(not(0)));
+    }
+
+    @Test
+    void best_books_returns_empty_json_while_wrong_hoursDaily() {
+        when()
+                .get("/books/pages/{pagesPerHour}/hours/{hoursDaily}", 5, 25)
+                .then()
+                .statusCode(200)
+                .body("", hasSize(0));
+    }
+}
--- src/test/java/com/library/service/BookServiceImplTest.java
+++ src/test/java/com/library/service/BookServiceImplTest.java
@@ -10,9 +10,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Optional;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
@@ -28,31 +26,59 @@ class BookServiceImplTest {
     BookServiceImpl bookService;
 
     final Book BOOK_1 = new Book("1", "Test", "book1", "Tester", 2021L, "Test book1"
-            , 20, "http://test1.pl", "PL", "http://prev.pl", 5.0, null, null);
+            , 200, "http://test1.pl", "PL", "http://prev.pl", 5.0, null, null);
 
     final Book BOOK_2 = new Book("2", "Test", "book2", "Tester", 2021L, "Test book2"
-            , 20, "http://test2.pl", "PL", "http://prev.pl", 5.0, null, null);
+            , 200, "http://test2.pl", "PL", "http://prev.pl", 5.0, null, null);
+
+    final List<Book> BOOK_LIST = Arrays.asList(BOOK_1, BOOK_2);
+
 
     @Test
-    void saveAll() {
-        when(bookRepository.saveAll(Arrays.asList(BOOK_1, BOOK_2))).thenReturn(Arrays.asList(BOOK_1, BOOK_2));
+    void saveAllTest() {
+        when(bookRepository.saveAll(BOOK_LIST)).thenReturn(BOOK_LIST);
 
-        assertThat(bookService.saveAll(Arrays.asList(BOOK_1, BOOK_2))).containsOnly(BOOK_1, BOOK_2);
-        assertEquals(2, bookService.saveAll(Arrays.asList(BOOK_1, BOOK_2)).size());
+        assertThat(bookService.saveAll(BOOK_LIST)).containsOnly(BOOK_1, BOOK_2);
+        assertEquals(2, bookService.saveAll(BOOK_LIST).size());
     }
 
     @Test
-    void findBookByIsbn() {
+    void findBookByIsbnTest() {
         when(bookRepository.findById("1")).thenReturn(Optional.of(BOOK_1));
 
         assertEquals(Optional.of(BOOK_1), bookService.findBookByIsbn("1"));
     }
 
     @Test
-    void findBooksByCategory() {
-        when(bookRepository.findBooksByCategory("Computers")).thenReturn(Collections.singletonList(BOOK_1));
+    void findBooksByCategoryTest() {
+        when(bookRepository.findBooksByCategory("Computers")).thenReturn(BOOK_LIST);
 
-        assertNotEquals(Collections.singletonList(BOOK_2), bookService.findBooksByCategory("Computers"));
-        assertEquals(Collections.singletonList(BOOK_1), bookService.findBooksByCategory("Computers"));
+        assertEquals(BOOK_LIST, bookService.findBooksByCategory("Computers"));
     }
+
+    @Test
+    void findBookWithPagesGreaterThanTest() {
+        when(bookRepository.findTopBookByPageCountGreaterThan(100)).thenReturn(Optional.of(BOOK_1));
+
+        assertEquals(bookService.findBookWithPagesGreaterThan(100), Optional.of(BOOK_1));
+    }
+
+    @Test
+    void findBooksByPagesPerHourAndHoursDailyTest() {
+        final int PAGES_PER_HOUR = 20;
+        final int VALID_HOURS_DAILY = 5;
+        final int INVALID_HOURS_DAILY = 25;
+        final int DAYS = 30;
+
+        int validPagesPerMonth = PAGES_PER_HOUR * VALID_HOURS_DAILY * DAYS;
+
+
+        when(bookRepository.findBooksByPagesMonthly(validPagesPerMonth)).thenReturn(BOOK_LIST);
+
+        assertEquals(bookService.findBooksByPagesPerHourAndHoursDaily(PAGES_PER_HOUR, VALID_HOURS_DAILY),
+                BOOK_LIST);
+        assertEquals(bookService.findBooksByPagesPerHourAndHoursDaily(PAGES_PER_HOUR, INVALID_HOURS_DAILY),
+                new ArrayList<Book>());
+    }
+
 }
